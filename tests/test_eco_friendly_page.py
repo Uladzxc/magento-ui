8dd52e20a0267d19dc3c124f90385a227d59b4a4
Uladzislau Liavonau <uladzxliavonau@gmail.com>
2025-09-16 10:43:16 +0400

    tests exteneded with running options and allure

--- tests/test_eco_friendly_page.py
+++ tests/test_eco_friendly_page.py
@@ -1,15 +1,21 @@
+import pytest
+import allure
+@pytest.mark('regression')
+@allure.description('Check if grid type view is selected')
 def test_products_displayed_as_grid(eco_friendly_page):
     eco_friendly_page.open_page()
     eco_friendly_page.select_view("grid")
     eco_friendly_page.assert_display_type("grid", expected_display="block")
 
-
+@pytest.mark('regression')
+@allure.description('Check if list type view is selected')
 def test_products_displayed_as_list(eco_friendly_page):
     eco_friendly_page.open_page()
     eco_friendly_page.select_view("list")
     eco_friendly_page.assert_display_type("list", expected_display="block")
 
-
+@pytest.mark('smoke')
+@allure.description('Check if price options available')
 def test_select_price_option_in_the_dropdown_list(eco_friendly_page):
     eco_friendly_page.open_page()
     eco_friendly_page.select_element_from_the_dropdown_and_check_items_display(text="Price")
